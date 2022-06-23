752280e1b3841ae6c44cb1c2ed00b14dbf065bcb
Shivam <thisisshivam06@gmail.com>
2022-06-23 23:56:14 +0530

    Functional Components, Class Components, Hooks Update, JSX, Props, State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import Message from "./components/Message";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Message />
+        {/* <Greet name="Bruce" heroName="Batman">
+          <p>This is children props</p>
+        </Greet>
+        <Greet name="Clerk" heroName="Superman">
+          <button>Action</button>
+        </Greet>
+        <Greet name="Diana" heroName="Wonder Woman" />
+        <Welcome name="Bruce" heroName="Batman" />
+        <Welcome name="Clerk" heroName="Superman" />
+        <Welcome name="Diana" heroName="Wonder Woman" />
+        <Hello /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
