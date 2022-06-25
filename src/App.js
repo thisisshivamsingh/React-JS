9997eefbfa7ccf75d71d12d57490ef89414dd855
Shivam <thisisshivam06@gmail.com>
2022-06-26 01:07:01 +0530

    Lists and Keys, Index as Key Anti-Pattern, Styling and CSS Basics, Basics of Form Handling and Components Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -12,12 +12,22 @@ import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
+import Form from "./components/Form";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NameList />
+        <Form />
+        {/* <h1 className="error">Error</h1> */}
+        {/* <h1 className={styles.success}>Success</h1> */}
+        {/* <Inline /> */}
+        {/* <Stylesheet primary={true} /> */}
+        {/* <NameList /> */}
         {/* <UserGreeting /> */}
         {/* <ParentComponent /> */}
         {/* <EventBind /> */}
