0fa0bb0082eeaba3af07c530f7c024a5bcebfc87
Shivam <thisisshivam06@gmail.com>
2022-06-29 01:04:17 +0530

    Error Boundary and High Order Components(HOC)

--- src/App.js
+++ src/App.js
@@ -26,12 +26,27 @@ import RefsDemo from "./components/RefsDemo";
 import FocusInput from "./components/FocusInput";
 import FRParentInput from "./components/FRParentInput";
 import PortalDemo from "./components/PortalDemo";
+import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PortalDemo />
+        <ClickCounter name="Shivam" />
+        <HoverCounter />
+        {/* <ErrorBoundary>
+          <Hero heroName="Batman" />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName="Superman" />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName="Joker" />
+        </ErrorBoundary> */}
+        {/* <PortalDemo /> */}
         {/* <FRParentInput /> */}
         {/* <FocusInput /> */}
         {/* <RefsDemo /> */}
