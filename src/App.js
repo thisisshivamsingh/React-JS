6ccde90aab04838d42b25f9ce799d090065adedc
Shivam <thisisshivam06@gmail.com>
2022-07-01 01:08:36 +0530

    HTTP and React, HTTP GET Request and HTTP POST Request

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PostForm />
+        {/* <PostList /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
